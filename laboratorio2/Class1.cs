475e6b0fb3447331924d70de4a115f2063194880
Jean Pier Elias Valverde Zamora <jeanvalverdezamora@gmail.com>
2022-04-08 19:51:21 -0500

    Version 1

---
+++ laboratorio2/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorio2
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            // declaración de variables
+            double numero1, numero2, resultado = 0;
+            int opcion;
+
+            //Solicitar el ingreso por teclado
+            Console.WriteLine("╔══════════════════════════╗");
+            Console.WriteLine("║ Operaciones  Aritméticas ║");
+            Console.WriteLine("╚══════════════════════════╝");
+
+            Console.WriteLine("Ingrese Numero 1: ");
+            numero1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese Numero 2: ");
+            numero2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese opcion a realizar (1-4)");
+            opcion = int.Parse(Console.ReadLine());
+
+            // Realizando el calculo en función a la operación
+
+            if (opcion > 0 & opcion < 5)
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        resultado = numero1 + numero2;
+                        Console.WriteLine("La suma es :" + resultado);
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        resultado = numero1 - numero2;
+                        Console.WriteLine("La resta es :" + resultado);
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        resultado = numero1 * numero2;
+                        Console.WriteLine("La multiplicaicon es :" + resultado);
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        if (numero2 > 0)
+                        {
+                            resultado = numero1 / numero2;
+                            Console.WriteLine("La division es :" + resultado);
+                            Console.ReadKey();
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se puede hacer la division");
+                            break;
+                        }
+                }
+            }
+            else
+                {
+                 Console.WriteLine("opcion invalida");
+                 Console.ReadKey();
+                        
+                }
+
+            }
+        }
+    }
+
+
+
+
+    
+    
+
---
+++ laboratorio2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string estadocivil;
+            int edad;
+            double salario;
+
+            Console.WriteLine("Ingresa el estado civil");
+            estadocivil = System.Console.ReadLine();
+
+            Console.WriteLine("Ingresa su edad");
+            edad = int.Parse(System.Console.ReadLine());
+
+            Console.WriteLine("Ingresa su salario");
+            salario = double.Parse(System.Console.ReadLine());
+
+            //realizar la funcionalidad
+
+            if (estadocivil.Equals("casado") && edad >= 18 && salario > 2500)
+            {
+                System.Console.WriteLine("Prestamo aprobado");
+            }
+            else if (estadocivil.Equals("casado") && edad >= 18 && (salario > 900 && salario <= 2500))
+            {
+                System.Console.WriteLine("Prestamo no aprobado");
+            }
+            else
+            {
+                System.Console.WriteLine("No cumples con las condiciones para solicitar un prestamo");
+            }
+            //impresion en pantalla
+            Console.ReadKey();
+        }
+    }
+}
